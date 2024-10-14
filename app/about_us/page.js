32001168a3ee83184c7940bdb23df5321f3af088
Lu Lu <lulu@Lu.local>
2024-10-14 13:28:40 +0200

    added our partner component into page.js

--- app/about_us/page.js
+++ app/about_us/page.js
@@ -1,4 +1,7 @@
-import styles from './page.module.css';
+import styles from "./page.module.css";
+import OurValues from "./OurValues";
+import OurCrew from "./OurCrew";
+import OurPartners from "./OurPartners";
 // TASK - React 1 week 1
 // After you are finished with creating the page, move the OurValues, OurCrew, OurPartners components into their own files
 // OurValues.js, OurCrew.js, OurPartners.js should live in this folder
@@ -8,21 +11,21 @@ export const Crew = () => {
   return (
     <div className="fullBGpicture">
       <main className="mainContent">
-        <h1>About us</h1>
+        <h2>About us</h2>
         <section className="card">
           <h2>Our Values</h2>
-          <OurValues/>
+          <OurValues />
         </section>
         <section className="card">
           <h2>The crew</h2>
-          <OurCrew/>
+          <OurCrew />
         </section>
-
-         {/* TASK - React 1 week 1 */}
-         {/* Add in the "OurPartners" component here */}
+        <OurPartners />
+        {/* TASK - React 1 week 1 */}
+        {/* Add in the "OurPartners" component here */}
       </main>
     </div>
   );
-}
+};
 
 export default Crew;
