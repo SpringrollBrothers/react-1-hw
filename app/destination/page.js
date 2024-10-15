63589dc73ee4bbdabc2947cc63323efce11d7d65
Lu Lu <lulu@Lu.local>
2024-10-15 21:15:27 +0200

    moved planetwishlistitem to seprate files

--- app/destination/page.js
+++ app/destination/page.js
@@ -7,19 +7,7 @@ import { AddWishlistItem } from '@/components/destination/AddWishlistItem';
 
 // TASK - React 1 week 2
 // Move this to its own file
-const PlanetWishlistItem = ({
-  name,
-  onRemove,
-  thumbnail,
-}) => {
-  return (
-    <div className={styles.wishlistItem}>
-      <img className={styles.wishlistItemThumbnail} src={thumbnail} alt="" />
-      <b>{name.toUpperCase()}</b>
-      <button onClick={onRemove}>remove</button>
-    </div>
-  );
-}
+
 
 
 export const Destinations = () => {
